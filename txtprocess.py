70e82c0db8aa8b1f12d8a5696851ef28fbea9f5d
Blake Boswell <j.blake.boswell@gmail.com>
2016-06-05 21:10:53 -0400

    trying to use all streaming

--- txtprocess.py
+++ txtprocess.py
@@ -1,6 +1,8 @@
 import re
 import string
+import json
 import sklearn as sk
+import numpy as np
 from nltk.corpus import stopwords
 from sklearn.base import TransformerMixin
 from sklearn.base import BaseEstimator
@@ -42,6 +44,27 @@ class KeySelect(TransformerMixin):
         return self
 
 
+class FileIterKeySelect(TransformerMixin):
+    ''' select key from dictionary
+    '''
+    def __init__(self, xkey='content', ykey='label'):
+        self.xkey = xkey
+        self.ykey = ykey
+
+    def _json_load(self, line):
+        return json.loads(line)
+
+    def _select_keys(self, element):
+        return element[self.xkey], element[self.ykey]
+
+    def transform(self, f, **transform_params):
+        x, y = zip(*[self._select_keys(self._json_load(line)) for line in f])
+        yield x, y
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+
 class StripTransform(TransformerMixin):
     ''' transform raw content to only word chars
     '''
@@ -60,9 +83,12 @@ class CosineSim(BaseEstimator):
         return self
 
     def fit(self, X, y=None, **fit_params):
-        self.X = X
+        indices = np.array([e for e in y if e == 2]).nonzero()[0]
+        self.X = X[indices, :]
 
     def predict(self, X, y=None, **predict_params):
-        return pairwise_distances(self.X, X, metric='cosine')
+        d = pairwise_distances(self.X, X, metric='cosine')
+        d = d / d.sum(axis=1)[:, np.newaxis] #normalize
+        return d.sum(axis=0)
 
 
