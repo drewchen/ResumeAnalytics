677728cff9442e9e182e3fb818c79b015f26c631
Andrew Chen <chen_andrew@bah.com>
2016-06-13 12:30:03 -0400

    Latest checkin, merged the docx to txt code with the pdf to txt code

--- dirtojsonl.py
+++ dirtojsonl.py
@@ -6,6 +6,38 @@ import zipfile
 import os
 import json
 
+"""
+Module that extract text from MS XML Word document (.docx).
+(Inspired by python-docx <https://github.com/mikemaccana/python-docx>)
+"""
+
+WORD_NAMESPACE = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
+PARA = WORD_NAMESPACE + 'p'
+TEXT = WORD_NAMESPACE + 't'
+
+def _docx_to_txt(path):
+    """
+    Take the path of a docx file as argument, return the text in unicode.
+    """
+    document = zipfile.ZipFile(path)
+    xml_content = document.read('word/document.xml')
+    document.close()
+    tree = XML(xml_content)
+
+    paragraphs = []
+    for paragraph in tree.getiterator(PARA):
+        texts = [node.text
+                 for node in paragraph.getiterator(TEXT)
+                 if node.text]
+        if texts:
+            paragraphs.append(''.join(texts))
+
+    return '\n\n'.join(paragraphs)
+
+"""
+End Module
+"""
+
 def _pdf_to_txt(root_dir):
     ''' convert all pdfs in root_dir to text including sub dirs
     '''
@@ -34,12 +66,19 @@ def _txt_to_jsonl(root_dir, outpath):
                     json.dump({'name': f, 'content': inf.read()}, outf)
                     outf.write('\n')
             os.remove(path)
+        ## Also here, add all found docx files to the outf file
+        for root, _, files in os.walk(root_dir):
+            for f in files:
+                _, ext = os.path.splitext(f)
+                if ext == '.doc' or ext == '.docx':
+                    json.dump({'name': f, 'content': _docx_to_txt(os.path.join(root, f))}, outf)
+                    outf.write('\n')
 
 
-def process_pdfs(pdf_dir, out_path):
+def process_directory(in_dir, out_path):
     ''' wrap it all up
     '''
-    _pdf_to_txt(pdf_dir)
-    _txt_to_jsonl(pdf_dir, out_path)
+    _pdf_to_txt(in_dir)
+    _txt_to_jsonl(in_dir, out_path)
 
 
